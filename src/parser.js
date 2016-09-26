9cdad5f54ea7ca3fb8d868aae0a2124210d926fb
David Edgar <david.r.edgar@gmail.com>
2016-09-26 18:31:32 +0100

    Parse elements into tree.

--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,6 @@
 
 
-function Node(value) {
+var Node = function(value) {
 
     this.value = value;
     this.children = [];
@@ -26,11 +26,89 @@ function Node(value) {
     this.removeChildren = function() {
         this.children = [];
     }
+
+    this.getTotalDescendantCount = function() {
+        var count = 0;
+        for (child of this.children) {
+            count += child.getTotalDescendantCount();
+        }
+        return count + this.children.length;
+    }
 }
 
 
-//now need specific tree with id, type, and value
-//(for m, use value to determine further elements, don't add to node)
+var PrBufNode = function(id, type, val) {
+    this.value = {id, type, val}
+    this.children = [];
+    this.parent = null;
+}
+
+PrBufNode.prototype = new Node();
+PrBufNode.prototype.constructor = PrBufNode;
+
+//Compares the number of descendants with the value specified in the map element.
+//If all the children have not yet been added, we continue adding to this element.
+PrBufNode.prototype.findLatestIncompleteNode = function() {
+
+    //if it's a branch (map) node ('m') and has room, return this node
+    if ((this.value.type === 'm') && (this.value.val > this.getTotalDescendantCount())) {
+        return this;
+    }
+    else {
+        //if we've reached the root, return null
+        if (this.parent === null) {
+            return null;
+        } else {
+            //otherwise recurse up the tree
+            return this.parent.findLatestIncompleteNode();
+        }
+    }
+}
+
+
+
+//parses the input URL 'data' protocol buffer parameter into a tree
+var parse = function(urlToParse) {
+    var root = null;
+    var re = /data=!(.*)\?/
+    var dataArray = urlToParse.match(re);
+    if (dataArray && dataArray.length >= 1) {
+        //console.log(dataArray[1]);
+        var elemArray = dataArray[1].split("!");
+        $("#parseResult").text(elemArray);
+        //now iterate through elements, placing them at appropriate location in tree
+
+        var workingNode = null;
+
+        //we iterate through each of the elements, creating a node for it, and
+        //deciding where to place it in the tree
+        for (var i=0; i < elemArray.length; i++) {
+            var elemRe = /^([0-9])([a-z])(.*)$/
+            var elementCompositionArray = elemArray[i].match(elemRe);
+            if (elementCompositionArray && elementCompositionArray.length > 3) {
+                //console.log(elementCompositionArray);
+                var id = elementCompositionArray[1];
+                var type = elementCompositionArray[2];
+                var value = elementCompositionArray[3];
+                var elemNode = new PrBufNode(id, type, value);
+                if (!root) {
+                    root = elemNode;
+                    workingNode = root;
+                } else {
+                    workingNode.addChild(elemNode);
+                    workingNode = elemNode.findLatestIncompleteNode();
+                    if (null == workingNode) {
+                        //we've filled up the branches right back to the root, so nothing more we can add
+                        break;
+                    }
+                }
+            }
+        }
+        console.log(root);
+    }
+
+}
+
 
 
 
@@ -38,20 +116,10 @@ function Node(value) {
 $(document).ready(function() {
     var inputURL = $("#inputURL").bind("input", function() {
 
-        var that = this;
-        setTimeout(function() {
-            var re = /data=!(.*)\?/
-            var dataArray = that.value.match(re);
-            if (dataArray && dataArray.length >= 1) {
-                //console.log(dataArray[1]);
-                var elemArray = dataArray[1].split("!");
-                $("#parseResult").text(elemArray);
-                //now iterate through elements, placing them at appropriate location in tree
+        var urlToParse = this.value;
+        setTimeout(parse(urlToParse), 500);
 
-                var root = new Node('root');
-            }
+    });
 
-        }, 500);
-
-  });
+    parse($("#inputURL")[0].value);
 });
